ae369c500d9016345697da3177ced137b5a21848
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-15 20:14:38 +0530

    generated schema

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,3 +2,27 @@
 
 package model
 
+type CreateJobListingInput struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Company     string `json:"company"`
+	URL         string `json:"url"`
+}
+
+type DeleteJobResponse struct {
+	DeleteJobID string `json:"deleteJobID"`
+}
+
+type JobListing struct {
+	ID          string `json:"_id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Company     string `json:"company"`
+	URL         string `json:"url"`
+}
+
+type UpdateJobListingInput struct {
+	Title       *string `json:"title,omitempty"`
+	Description *string `json:"description,omitempty"`
+	URL         *string `json:"url,omitempty"`
+}
